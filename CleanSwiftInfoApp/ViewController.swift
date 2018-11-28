72a41e14ff7eed1cd8ecb040a28132542e4f26b5
Yani Buchkov <Yani.Buchkov@adastragrp.com>
2018-11-28 14:55:58 +0200

    Initial Commit

---
+++ CleanSwiftInfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftInfoApp
+//
+//  Created by Yani Buchkov on 28.11.18.
+//  Copyright © 2018 Yani Buchkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
